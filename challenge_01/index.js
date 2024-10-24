f59a276f25ab14a23245fa390eff704bd7955285
Milder <milderz@gmail.com>
2024-10-24 10:28:31 -0600

    add fetch

--- challenge_01/index.js
+++ challenge_01/index.js
@@ -1,3 +1,9 @@
-console.log("hello node")
+const fetchData = async () => {
+    const res = await fetch('https://codember.dev/data/message_01.txt')
+    const data = await res.json()
+    return data
+}
 
-// new line
+const data = fetchData();
+
+console.log(data)
